3409aa375a8c04be794b498d65cd36e5713447ea
Thomas Flemming <thomas.flemming@usit.uio.no>
2010-07-07 11:45:31 +0200

    modified examples

--- examples/create_collection.rb
+++ examples/create_collection.rb
@@ -5,6 +5,12 @@ include Vortex
 vortex = Vortex::Connection.new("https://vortex-dav.uio.no/")
 vortex.cd('/brukere/thomasfl/events/')
 
+# Create standard folder
+collection = Collection.new(:url => 'my-collection', :title => 'My articles')
+path = vortex.create(collection)
+puts "Created folder: " + path
+
+# Create standard
 collection = ArticleListingCollection.new(:url => 'my-collection', :title => 'My articles')
 path = vortex.create(collection)
 puts "Created folder: " + path
