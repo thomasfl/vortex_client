50d361ba0c996d938d8d96beee5269ab9b238afc
Thomas Flemming <thomas.flemming@usit.uio.no>
2010-07-08 11:37:53 +0200

    shortened example

--- examples/upload_image.rb
+++ examples/upload_image.rb
@@ -2,17 +2,8 @@ require 'rubygems'
 require 'vortex_client'
 
 # Upload a binary file to Vortex
-
-def get_file_as_string(filename)
-  data = ''
-  f = File.open(filename, "r")
-  f.each_line do |line|
-    data += line
-  end
-  return data
-end
-
 vortex = Vortex::Connection.new("https://vortex-dav.uio.no/")
 vortex.cd('/brukere/thomasfl/')
 content = get_file_as_string("dice.gif")
+content = open("dice.gif", "rb") {|io| io.read }
 vortex.put_string("dice_6.gif", content)
