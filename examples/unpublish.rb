413bbf02e231b5c32c87b9d9b75bf81b8c815c76
Thomas Flemming <thomas.flemming@usit.uio.no>
2010-10-18 11:26:33 +0200

    add comments

--- examples/unpublish.rb
+++ examples/unpublish.rb
@@ -1,7 +1,10 @@
 require 'rubygems'
 require 'vortex_client'
 
-# Find all files recursively under '/people' with name 'index.html', and set them to "unpublished"
+# Hides documents for visitors by setting the documents unpublish-date to current time and date.
+#
+# Find all files recursively under '/people' with name 'index.html', and set "unpublished-date" to now.
+
 @vortex = Vortex::Connection.new("https://nyweb2-dav.uio.no/", :use_osx_keychain => true)
 @vortex.find('/people/', :recursive => true, :filename=>/\.html$/) do |item|
   item.proppatch('<v:unpublish-date xmlns:v="vrtx">'+Time.now.httpdate.to_s+'</v:unpublish-date>')
