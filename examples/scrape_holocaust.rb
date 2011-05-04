bdc0c73925d501dc15d8dd27f706c8b5174a6494
Thomas Flemming <thomas.flemming@gmail.com>
2011-05-04 15:40:32 +0200

    download and rescale multiple images

--- examples/scrape_holocaust.rb
+++ examples/scrape_holocaust.rb
@@ -19,18 +19,20 @@ require 'json'
 #  ok - Kopiere over ingress bilde
 #  ok - Luke ut rare tegn i overskriftene
 #  ok - Kjøre alle dokumenter uten kræsj
-#  - Kopiere over flere bilder
+#  ok - Kopiere over flere bilder
 #  ok - Få logging til fil til å fungere
+# - Logg publisering til fil
 # - Hvorfor konverteres og publiseres /konv/kunnskapsbasen/-a-hrefhttp-.html
 # - Håndtere /konv/kunnskapsbasen/hl-senterets-kunnskapsbase.html spesielt?
 
 @vortex = Vortex::Connection.new("https://nyweb4-dav.uio.no", :use_osx_keychain => true)
 
-# Log to file
+# Simple logger
 def log(str)
   puts str
-  log_str = Time.now.iso8601 + ";" + str + "\n"
-  File.open("scrape_holocaust.log", 'a') {|f| f.write(log_str) }
+  File.open("scrape_holocaust.log", 'a') do |f|
+    f.write( Time.now.iso8601 + ";" + str + "\n" )
+  end
 end
 
 def http_content_type(url)
@@ -45,6 +47,24 @@ def http_content_type(url)
   return response['content-type']
 end
 
+def scrape_folder_title(url)
+  begin
+    doc = Nokogiri::HTML.parse(open(scrape_url))
+    doc.encoding = 'utf-8'
+
+    title = nil
+    begin
+      title = doc.css(".folder .title").first.inner_html
+    rescue
+    end
+    if(title == nil)then
+      title = doc.css(".article .title").first.inner_html
+    end
+  rescue
+  end
+  return title
+end
+
 def create_path(dest_path)
   destination_path = "/"
   dest_path.split("/").each do |folder|
@@ -57,71 +77,85 @@ def create_path(dest_path)
         puts "Creating folder " + destination_path
 
         uri = URI.parse(@url)
-        scrape_url = 'http://' + uri.host + destination_path.gsub('/konv','')
-        doc = Nokogiri::HTML.parse(open(scrape_url))
-        doc.encoding = 'utf-8'
 
-        title = nil
-        begin
-          title = doc.css(".folder .title").first.inner_html
-        rescue
-        end
-        if(title == nil)then
-          title = doc.css(".article .title").first.inner_html
-        end
+        title = scrape_folder_title( 'http://' + uri.host + destination_path.gsub('/konv','') )
 
-        if(title == nil)then
-          title = "Ingen tittel"
+        if(title)then
+          title = title[0..0].upcase + title[1..title.length]
+          puts "Mappetittel: " + title.to_s
         end
-        title = title[0..0].upcase + title[1..title.length]
-
-        puts "Mappetittel: " + title.to_s
 
         @vortex.mkdir(destination_path)
         @vortex.proppatch(destination_path,'<v:collection-type xmlns:v="vrtx">article-listing</v:collection-type>')
-        @vortex.proppatch(destination_path,'<v:userTitle xmlns:v="vrtx">' + title.to_s +  '</v:userTitle>')
+        if(title)then
+          @vortex.proppatch(destination_path,'<v:userTitle xmlns:v="vrtx">' + title.to_s +  '</v:userTitle>')
+        end
       end
     end
   end
 end
 
+
+# Return an array images and captions
+def scrape_images(doc)
+  images = []
+  i = 0
+  doc.css(".imageSeriesImage").each do |item|
+    url = item.attr("src")
+    caption = doc.css(".imageText")[i].text
+    images << { :url => url, :caption => caption }
+    i = i + 1
+  end
+  return images
+end
+
+
+# Resize an image using the unix command line utility 'sips' available on osx
+def resize_image(content, content_type, size)
+  filename = "/tmp/" + (1 + rand(10000000)).to_s + "." + content_type
+  filename_resized = "/tmp/" + (1 + rand(10000000)).to_s + "_resized." + content_type
+  File.open(filename, 'w') do |f|
+    f.write( content)
+  end
+  result = %x[sips --resampleWidth #{size} #{filename} --out #{filename_resized}]
+  content_resized = IO.readlines(filename_resized,'r').to_s
+  return content_resized
+end
+
+
+# Download graphic to dest_path and return absolute filename
+def download_image(src_url,dest_path)
+  src_url = src_url.gsub(/\?.*/,'')
+  content_type = http_content_type(src_url)
+  content_type = content_type.gsub("image/", "").gsub("jpeg","jpg")
+  content = open(src_url).read
+  basename = Pathname.new(src_url).basename.to_s.gsub(/\..*/,'')
+  vortex_url = dest_path + basename + "." + content_type
+  @vortex.put_string(vortex_url, content)
+
+  # Store a resized image to vortex
+  puts "Nedskalerer bilde: " + src_url
+  content_resized = resize_image(content, content_type,300)
+  vortex_url_resized = dest_path + basename + "_width_300." + content_type
+  @vortex.put_string(vortex_url_resized, content_resized)
+  return { :vortex_url_resized => vortex_url_resized, :vortex_url => vortex_url}
+end
+
+
 def publish_article(path, title, introduction, body, doc)
-  puts "Publishing..."
+  # puts "Publishing..."
   dest_path = "/konv" + path.gsub(/\d*$/,'')
   puts dest_path
   create_path(dest_path)
   @vortex.cd(dest_path)
 
-  new_image_url = nil
-  article_image_url = nil
-  caption = nil
-  begin
-    article_image_url = doc.css(".imageSeriesImage").first.attr("src")
-    caption = doc.css(".imageText").first.text
-    puts "Bildetekst:" + caption
+#   images = scrape_images(doc)
+  images = []
 
-    image_count = doc.css(".articleImageText").size
-    puts "DEBUG: antall bilder: " + image_count.to_s
-    if(image_count == 2)then
-
-    end
-    if(image_count > 2)then
-      puts "Flere enn 2 bilder!"
-      # exit
-    end
-
-  rescue
-  end
-  if(article_image_url)then
-    image_content = open(article_image_url)
-    article_image_url = article_image_url.gsub(/\?.*/,'')
-    content_type = http_content_type(article_image_url)
-
-    content_type = content_type.gsub("image/", "").gsub("jpeg","jpg")
-    article_image_content = open(article_image_url).read
-    basename = Pathname.new(article_image_url).basename.to_s.gsub(/\..*/,'')
-    new_image_url = dest_path + basename + "." + content_type
-    @vortex.put_string(new_image_url, article_image_content)
+  images.each do |image|
+    filenames = download_image(image[:url], dest_path)
+    image[:vortex_url] = filenames[:vortex_url_resized]
+    image[:vortex_url_org] = filenames[:vortex_url]
   end
 
   attributes = {:title => title,
@@ -129,17 +163,49 @@ def publish_article(path, title, introduction, body, doc)
     :body => body,
     :publishedDate => Time.now}
 
-  if(new_image_url)then
-    attributes[:picture] = new_image_url
+  if(images.first)then
+    attributes[:picture] = images.first[:vortex_url]
   end
 
   article  = Vortex::StructuredArticle.new(attributes )
   url = @vortex.publish(article)
 
-  if(caption)then
+  # Add additional images to bottom of page
+  images_html = ""
+  if(images.size > 1)then
+    images[1..images.size].each do |image|
+      image_html = <<EOF
+        <p>
+          <div class="vrtx-introduction-image" style="width: 300px; ">
+            <a title="Last ned bilde i full størrelse" href="#{image[:vortex_url_org]}">
+              <img src="#{image[:vortex_url]}" style="width: 300px;" />
+            </a>
+            <div class="vrtx-imagetext">
+              <div class="vrtx-imagedescription">
+                #{image[:caption]}
+              </div>
+            </div>
+          </div>
+        </p>
+EOF
+      images_html = images_html + image_html
+    end
+  end
+
+  # Reopen document and set caption on article image
+  if(images.first)then
     @vortex.find(url) do |item|
       data = JSON.parse(item.content)
+
+      caption = ""
+      if(images.first[:caption])then
+        caption = images.first[:caption]
+      end
+      caption = caption + " <a href=\"#{images.first[:vortex_url_org]}\">Last ned i full størrelse</a>"
       data["properties"]["caption"] = caption
+
+      # Add addiitional images at bottom
+      data["properties"]["content"] = data["properties"]["content"] + images_html
       item.content = data.to_json
     end
   end
@@ -163,26 +229,22 @@ def scrape_article(url)
   rescue
   end
 
-    body = ""
+  body = ""
   doc.css(".article .text").each do |p|
     body = body + "<p>" + p.inner_html + "</p>"
   end
 
   title = title.gsub('','–') # Fjern bindestrek
 
-  puts title
-  # puts "DEBUG: BODY:" + body
-  # puts "DEBUG: INTRODUCTION:" + introduction
-
-
-  puts
-  puts url
+  # puts title
+  # puts
+  # puts url
   uri = URI.parse(url)
   path = uri.path
-  puts path
-  published_path = publish_article(path, title, introduction, body,doc)
+  # puts path
+  published_path = publish_article(path, title, introduction, body, doc)
   log = path + ";" + URI.parse(published_path).path
-  puts
+  # puts
   File.open("scrape_holocaust.log", 'w') {|f| f.write(log) }
   puts title + " => " + published_path
 
@@ -197,59 +259,61 @@ def http_status_code(url)
 end
 
 def scrape_article_listing(url)
-  puts "Scraping article listing: " + url
+
   if( http_status_code(url) == 404)then
     puts "Advarsel: Status code 404: " + url
     return
   end
 
+  # Ad-hoc rule to ignore path
   if(url =~ /\/kunnskapsbasen\/Presse/)then
     puts "Advarsel: Ignorerer pressesidene har for store bilder for vortex: url"
     return
   end
 
   doc = Nokogiri::HTML.parse(open(url))
-
   doc.encoding = 'utf-8'
-  doc.css("div .list .article").each do |article|
-    href = article.css(".title a").attr("href").text
-    begin
-      href = href.gsub("%20","")
-    rescue
-    end
-    title = article.css(".title a").text
-    # puts title + " => " + href
-    if(href =~ /\d$/)then
-      scrape_article(href)
-    else
+
+  if(doc.css(".folder .list").size > 0 )then
+    puts "Scraping article listing page: " + url
+    doc.css(".folder .list .article").each do |article|
+      href = article.css(".title a").attr("href").text
+      begin
+        href = href.gsub("%20","")
+      rescue
+      end
       scrape_article_listing(href)
     end
+  else
+    # Pages without .folder .list is articles
+    puts "Scraping article: " + url
+    scrape_article(url)
   end
+
 end
 
-if @vortex.exists?('/konv/kunnskapsbasen/aktor/diktatorer/') then
-  @vortex.delete('/konv/kunnskapsbasen/aktor/diktatorer/')
+if @vortex.exists?('/konv/kunnskapsbasen/aktor') then
+  @vortex.delete('/konv/kunnskapsbasen/aktor')
 end
 @url = "http://www.hlsenteret.no/kunnskapsbasen/"
 
-scrape_article("http://www.hlsenteret.no/kunnskapsbasen/folkemord/armenerne/1334")
+scrape_article_listing(@url + "aktor/")
 exit
 
+# Denne har 4 bilder
+# scrape_article("http://www.hlsenteret.no/kunnskapsbasen/folkemord/armenerne/1334")
 
 # Denne har ingen tittel og skal ignoreres
 # scrape_article('http://www.hlsenteret.no/kunnskapsbasen/tradisjoner/86')
-# exit
 
 # Denne returnerer 404
 # scrape_article_listing("http://www.hlsenteret.no/kunnskapsbasen/tema/kunst")
 
 # Denne har bilder som er for store vortex
 # scrape_article_listing("http://www.hlsenteret.no/kunnskapsbasen/Presse/")
-# exit
 
 
 # scrape_article('http://www.hlsenteret.no/kunnskapsbasen/tema/relpol/10129')
-# exit
 
 scrape_article_listing(@url)
 
