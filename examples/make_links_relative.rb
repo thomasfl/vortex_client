d9a657c5753bef2d07d0370841a1016dbed28054
Thomas Flemming <thomas.flemming@gmail.com>
2011-07-01 14:26:41 +0200

    Added script to relativize links.

---
+++ examples/make_links_relative.rb
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+require 'rubygems'
+require 'find'
+require 'nokogiri'
+require 'vortex_client'
+require 'pathname'
+require 'pp'
+require 'json'
+
+def make_links_relative(dav_url)
+  vortex = Vortex::Connection.new(dav_url, :osx_keychain => true)
+  vortex.cd(URI.parse(dav_url).path.to_s)
+  count = 0
+  vortex.find('.', :recursive => true, :filename=>/\.html$/) do |item|
+    count = count + 1
+    puts "Checking    : " + count.to_s + ": " + item.url.to_s
+
+    json_data = JSON.parse(item.content)
+    content = json_data["properties"]["content"]
+    dirty = false
+    doc = Nokogiri::HTML(content)
+    doc.css('a').each do |element|
+      href = element['href']
+      if(href and href[/^\//])then
+        pathname_from = Pathname.new( URI.parse(item.url.to_s).path.to_s )
+        pathname_to   = Pathname.new( URI.parse(href.to_s).path.to_s )
+        new_href = pathname_to.relative_path_from(pathname_from.parent).to_s
+        element['href'] = new_href
+        puts "              " + href + " => " + new_href
+        dirty = true
+      end
+    end
+    if(dirty)then
+      json_data["properties"]["content"] = doc.to_s
+      item.content = json_data.to_json
+      puts "Updating    : " + item.url.to_s
+      puts
+    end
+  end
+
+end
+
+make_links_relative('https://www-dav.vortex-demo.uio.no/tmp/relativiser/')
