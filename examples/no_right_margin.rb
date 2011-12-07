7b1d8127168dfa18e44cb61de9ec9c609beb98ce
Thomas Flemming <thomas.flemming@gmail.com>
2011-12-07 13:21:46 +0100

    Updated existing example for compatibility with newer version of vortex

--- examples/no_right_margin.rb
+++ examples/no_right_margin.rb
@@ -1,11 +1,21 @@
+# -*- coding: utf-8 -*-
 require 'rubygems'
 require 'vortex_client'
+require 'json'
+require 'nokogiri'
 
-vortex = Vortex::Connection.new("https://www-dav.uio.no/")
+vortex = Vortex::Connection.new('https://www-dav.usit.uio.no', :osx_keychain => true)
 
-no_right_margin = '<hideAdditionalContent ' +
-  'xmlns="http://www.uio.no/vrtx/__vrtx/ns/structured-resources">true</hideAdditionalContent>'
-
-vortex.find('/konv/om/profil/', :recursive=>true)do|item|
-  item.proppatch(no_right_margin)
+# Skru av "Vis relatert innhold" på alle filer under en mappe.
+vortex.find('/om/organisasjon/sapp/dba/dokumentasjon/.', :recursive=>true)do|item|
+  if(item.type == :file and item.url.to_s[/\.html$/])
+    begin
+      data = JSON.parse(item.content)
+      data["properties"]["showAdditionalContent"] = "false"
+      item.content = data.to_json
+      puts "Updating: #{item.url.to_s}"
+    rescue Exception => exception
+      puts "Error: #{item.url.to_s} : #{exception.to_s}"
+    end
+  end
 end
